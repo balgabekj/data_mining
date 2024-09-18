5c1f702aa69d96475ecdea2933b0ebe9bb02b121
balgabekj <b_zhaksylyk@kbtu.kz>
2024-09-18 22:04:56 +0500

    upd

--- assignment1/5.py
+++ assignment1/5.py
@@ -4,10 +4,19 @@ df = pd.read_csv('Health_Sleep_Statistics.csv')
 
 df_no_dup = df.drop_duplicates()
 
-z_score = stats.zscore(df[['Age', 'Sleep Quality', 'Daily Steps', 'Calories Burned']])
-df_no_outliers = df[(z_score < 3).all(axis=1)]
-print(df_no_outliers.head())
+print(df_no_dup.head())
+
+# numeric_columns = ['Age', 'Sleep Quality', 'Daily Steps', 'Calories Burned']
+#
+# z_scores = stats.zscore(df[numeric_columns])
+#
+# z_score_df = pd.DataFrame(z_scores, columns=numeric_columns)
+#
+# print(z_score_df.head())
+
+# df_no_outliers = df[(abs(z_score_df) < 3).all(axis=1)]
+# print(df_no_outliers.head())
+
 
 df['Gender'] = df['Gender'].replace({'m': 'male', 'f': 'female'})
-
 print(df.head())
---
+++ assignment1/7.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+
+df = pd.read_csv('headbrain1.csv')
+
